a0570ef70f421fd157c026c65b1efdb7440b02a0
Eryk Napierała <eryk.piast@gmail.com>
2015-10-29 10:04:43 +0100

    Fix #2 by checking if file content is available

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ var through = require('through2');
 
 var GLOBAL_CACHE = {};
 
-module.exports = function (options) {
+module.exports = function(options) {
   options = options || {};
 
   var cache = options.cache || GLOBAL_CACHE;
   var firstPass = options.firstPass === true;
 
-  return through.obj(function (file, encoding, done) {
+  return through.obj(function(file, encoding, done) {
+    // null cannot be hashed
+    if (file.contents === null) {
+      // if element is really a file, something weird happened, but it's safer
+      // to assume it was changed (because we cannot said that it wasn't)
+      // if it's not a file, we don't care, do we? does anybody transform
+      // directories?
+      if (file.stat.isFile()) {
+        this.push(file);
+      }
+
+      return done();
+    }
+
     var newHash = crypto.createHash('sha1').update(file.contents).digest('hex');
     var currentHash = cache[file.path];
 
